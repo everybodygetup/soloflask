2eee4caaa4ad73782d71170e657add86c09466d1
Artem Belyakov <a9113621595@gmail.com>
2022-10-26 12:00:31 +0300

    Добавил новые шаблоны и пути к ним

--- app.py
+++ app.py
@@ -12,14 +12,9 @@ def index():
     return render_template('index.j2')
 
 
-@app.route('/index/')
-@app.route('/index/<name>')
-def hello(name=None):
-    return render_template('index.j2', name=name)
-
 
 @app.route('/about')
 def about():
-    return 'about page'
+    return render_template('about.j2')
 
 
