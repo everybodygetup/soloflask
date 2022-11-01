2da09cd9d6dd10b3d82867db8bfee398255113c6
Artem Belyakov <a9113621595@gmail.com>
2022-11-01 14:17:35 +0300

    Add new configuration

--- config.py
+++ config.py
@@ -8,3 +8,27 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    
+    SECURITY_CHANGEABLE = True
+    SECURITY_CONFIRMABLE = True
+    SECURITY_REGISTERABLE = True
+    SECURITY_RECOVERABLE = True
+    SECURITY_TRACKABLE = True
+
+    SECURITY_PASSWORD_LENGTH_MIN = 5
+    SECURITY_POST_LOGIN_VIEW = "/lk"
+    SECURITY_EMAIL_SENDER = ("Costravel", "mail@costravel.ru")
+
+    SECURITY_LOGIN_USER_TEMPLATE = "security/login.j2"
+    SECURITY_REGISTER_USER_TEMPLATE = "security/register.j2"
+    SECURITY_RESET_PASSWORD_TEMPLATE = "security/reset.j2"
+    SECURITY_FORGOT_PASSWORD_TEMPLATE = "security/forgot.j2"
+    SECURITY_CHANGE_PASSWORD_TEMPLATE = "security/change.j2"
+    SECURITY_SEND_CONFIRMATION_TEMPLATE = "security/confirm.j2"
+
+    SECURITY_PASSWORD_SALT = os.getenv("SECURITY_PASSWORD_SALT", "kasdasd123")
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+ 
