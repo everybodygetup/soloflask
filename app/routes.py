46c824030d50483973dcef636cdb0be4d470e4bf
Artem Belyakov <a9113621595@gmail.com>
2022-11-01 14:16:33 +0300

    Перенес роуты во вьюхи

--- app/routes.py
+++ app/routes.py
@@ -9,60 +9,40 @@ from flask_security import current_user
 
 
 
-@app.route('/about')
-def about():
-    return render_template('about.j2')
 
 
 
-@app.route('/ved',methods=['POST','GET'])
-def ved():
-    """Создание бд кодов только из модели напрямую"""
-    if request.method == "POST":
-        title = request.form['title']
-        intro = request.form['intro']
-        text = request.form['text']
-
-        ved_db = TnVed(title=title,intro=intro,text=text)
-
-        try:
-            db.session.add(ved_db)
-            db.session.commit()
-            flash("Код ТНВЭД добавлен", "success")
-            return redirect('/posts')
-        except:
-            return "Произошла ошибка"
-    return render_template('ved.j2')
 
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
-    return render_template('login.j2', title='Sign In', form=form)
+
+#@app.route('/login', methods=['GET', 'POST'])
+#def login():
+#    form = LoginForm()
+#    if form.validate_on_submit():
+#        flash('Login requested for user {}, remember_me={}'.format(
+#            form.username.data, form.remember_me.data))
+#       return redirect(url_for('index'))
+#    return render_template('login.j2', title='Sign In', form=form)
 
 
 
 #@app.route("/ved2", methods=["GET", "POST"])
 #@roles_accepted("admin")
 #def ved2():
-    """Создание бд кодов."""
-    ved_form = VedForm(obj=request.form)
-
-    if ved_form.validate_on_submit():
-        vd_db = TnVed(
-            title=request.form.get("title"),
-            text=request.form.get("text")
-        )
-        db.session.add(vd_db)
-        db.session.commit()
-        flash("Код ТНВЭД добавлен", "success")
-        return redirect(url_for("posts", id_feedback=vd_db.id))
-    return render_template("ved2.j2", form=ved_form)
+# """Создание бд кодов."""
+#    ved_form = VedForm(obj=request.form)
+#
+#   if ved_form.validate_on_submit():
+#        vd_db = TnVed(
+#            title=request.form.get("title"),
+#            text=request.form.get("text")
+#        )
+#        db.session.add(vd_db)
+#        db.session.commit()
+#        flash("Код ТНВЭД добавлен", "success")
+#        return redirect(url_for("posts", id_feedback=vd_db.id))
+#    return render_template("ved2.j2", form=ved_form)
 
 
 
