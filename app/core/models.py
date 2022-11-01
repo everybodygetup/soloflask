91e9e1d3e296b80b572a2d15eab6ba52b28bbae8
Artem Belyakov <a9113621595@gmail.com>
2022-11-01 14:13:52 +0300

    Перенес модели в core

---
+++ app/core/models.py
@@ -0,0 +1,44 @@
+from datetime import datetime
+from extensions import db
+from flask_security import SQLAlchemyUserDatastore
+from flask_security.models.fsqla_v2 import FsModels, FsRoleMixin, FsUserMixin
+
+
+
+FsModels.set_db_info(db)
+
+
+
+class Role(db.Model, FsRoleMixin):
+    pass
+
+
+class User(db.Model, FsUserMixin):
+    posts = db.relationship("Post", backref="author", lazy="dynamic")
+    firstname = db.Column(db.String(255))
+    lastname = db.Column(db.String(255))
+    phone = db.Column(db.String(20))
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(140))
+    text = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+
+    def __repr__(self):
+        return f"<Post {self.title}>"
+
+
+class TnVed(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), primary_key=False)
+    intro = db.Column(db.String(300), primary_key=False)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<TnWed %r>' % self.id
+
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
