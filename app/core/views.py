d0f0eaf402b4acdb3513c37b79798a93dafe2b49
Artem Belyakov <a9113621595@gmail.com>
2022-11-01 14:16:15 +0300

    Refactoring views

--- app/core/views.py
+++ app/core/views.py
@@ -1,6 +1,5 @@
-from flask import Blueprint,request,render_template,redirect
+from flask import Blueprint,request,render_template,redirect,flash
 from app.extensions import db
-
 
 core = Blueprint('core', __name__, template_folder='templates')
 
@@ -16,3 +15,29 @@ def create_tables():
 def index():
     '''Главная страница'''
     return render_template('index.j2')
+
+
+
+@core.route('/about')
+def about():
+    return render_template('about.j2')
+
+
+@core.route('/ved',methods=['POST','GET'])
+def ved():
+    """Создание бд кодов только из модели напрямую"""
+    if request.method == "POST":
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
+
+        ved_db = TnVed(title=title,intro=intro,text=text)
+
+        try:
+            db.session.add(ved_db)
+            db.session.commit()
+            flash("Код ТНВЭД добавлен", "success")
+            return redirect('/posts')
+        except:
+            return "Произошла ошибка"
+    return render_template('ved.j2')
