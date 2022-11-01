073314deefe6deb6aa88dbb65f6ddbedccec87c5
Artem Belyakov <a9113621595@gmail.com>
2022-11-01 14:17:07 +0300

    Refacotoring init

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,20 @@
 from flask import Flask
 from config import Config
-from app.extensions import db,migrate
+from extensions import babel, db, executor, mail, migrate, security
 from app.posts import posts
 from app.core import core
+from app.core.models import user_datastore
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db.init_app(app)
 migrate.init_app(app, db)
+security.init_app(app,user_datastore)
+
 
 app.register_blueprint(posts)
 app.register_blueprint(core)
 
 
-from app import routes, models
