a11655a4101868bed0da0f78ae74e4a0331f3cce
Artem Belyakov <a9113621595@gmail.com>
2022-11-03 13:37:36 +0300

    Добавил форму поиска

---
+++ app/core/forms.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField,TextAreaField
+from wtforms.validators import DataRequired
+
+
+class SpeciesForm(FlaskForm):
+    find = StringField('слово или код', validators=[DataRequired(message='Обязательное поле')])
+    findButton = SubmitField('найти')
