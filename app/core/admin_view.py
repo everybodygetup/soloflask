e8d82b6acc335176e5ddee8cedc70ec959d6ef1e
Artem Belyakov <a9113621595@gmail.com>
2022-11-11 13:06:57 +0300

    Вьюхи админки

--- app/core/admin_view.py
+++ app/core/admin_view.py
@@ -10,8 +10,36 @@ admin = Blueprint("admin", __name__, template_folder="templates")
 
 
 @admin.get("/admin")
-#@login_required
-#@permissions_required("admin-read")
+#login_required
+#permissions_required("admin-read")
 def index():
     """Главная страница админки."""
     return render_template("admin/index.j2")
+
+
+@admin.get("/users")
+#roles_required("admin")
+def users():
+    """Показ всех пользователей сайта."""
+    user_list_db = User.query.all()
+    return render_template("admin/users.j2", users=user_list_db)
+
+
+@admin.get("/user/<int:user_id>/roles")
+#roles_required("admin")
+def user_roles(user_id):
+    """Показываем роли для конкретного пользователя."""
+    user_db = User.query.get_or_404(user_id)
+    roles_db = Role.query.all()
+    return render_template("admin/roles.j2", user=user_db, roles=roles_db)
+
+
+@admin.get("/user/<int:user_id>/<int:role_id>/add")
+#roles_required("admin")
+def user_role_add(user_id, role_id):
+    """Добавление роли пользователю."""
+    user_db = User.query.get_or_404(user_id)
+    role_db = Role.query.get_or_404(role_id)
+    user_datastore.add_role_to_user(user_db, role_db)
+    db.session.commit()
+    return redirect(url_for("admin.user_roles", user_id=user_id))
