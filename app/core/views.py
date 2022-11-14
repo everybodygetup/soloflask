c2606ea54bb5555cc8f5249bd6dd05ad82c39669
Artem Belyakov <a9113621595@gmail.com>
2022-11-14 13:14:59 +0300

    Вьюха обратной связи

--- app/core/views.py
+++ app/core/views.py
@@ -1,7 +1,10 @@
 from flask import Blueprint, request, render_template, redirect, flash, g
+from flask_security import current_user
 from app.extensions import db
 from app.parts.models import SpareParts
-from app.core.forms import KodForm
+from app.core.models import UserSubmit
+from app.core.forms import KodForm, FeedbackForm
+
 
 core = Blueprint('core', __name__, template_folder='templates')
 
@@ -23,7 +26,20 @@ def index():
 
 @core.route('/about')
 def about():
-    return render_template('about.j2')
+    """Главная страница сайта."""
+    feedback_form = FeedbackForm(obj=request.form)
+
+    if feedback_form.validate_on_submit():
+        core_db = UserSubmit(
+            fist_name=request.form.get("first_name"),
+            last_name=request.form.get("last_name"),
+            email=request.form.get("email"),
+            text=request.form.get("text"),
+            user_id=current_user.id,
+        )
+        core_db.save()
+    
+    return render_template('about.j2', form=feedback_form)
 
 
 @core.route('/lk')
