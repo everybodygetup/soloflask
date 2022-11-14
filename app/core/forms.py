5adb60562c97c254783b0de0c2a6fad17d9468f0
Artem Belyakov <a9113621595@gmail.com>
2022-11-14 13:15:22 +0300

    Добавил форму обратной связи

--- app/core/forms.py
+++ app/core/forms.py
@@ -1,8 +1,25 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField, TelField, EmailField
+from wtforms.validators import DataRequired, Email
 
 
 class KodForm(FlaskForm):
     find = StringField('слово или код', validators=[DataRequired(message='Обязательное поле')])
     findButton = SubmitField('найти')
+
+
+class FeedbackForm(FlaskForm):
+    """Форма отзыва."""
+
+    first_name = StringField("Имя")
+    last_name = StringField("Фамилия")
+    phone = TelField("Номер телефона")
+    email = EmailField(
+        "Email",
+        validators=[
+            DataRequired("Email обязателен для заполнения"),
+            Email("Email введён неправильно"),
+        ],
+    )
+    text = TextAreaField("Сообщение", validators=[DataRequired("Сообщение обязательно для заполнения")])
+    submit = SubmitField("Отправить")
