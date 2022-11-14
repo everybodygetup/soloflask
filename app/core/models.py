64511d1d7c847e146423b83524ae4b72c92eb1f0
Artem Belyakov <a9113621595@gmail.com>
2022-11-14 13:15:10 +0300

    БД для обратной связи

--- app/core/models.py
+++ app/core/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from app.extensions import db
+from app.models import ModelMixin
 from flask_security import SQLAlchemyUserDatastore
 from flask_security.models.fsqla_v2 import FsModels, FsRoleMixin, FsUserMixin
 
@@ -21,6 +22,16 @@ class User(db.Model, FsUserMixin):
         backref=db.backref('users', lazy='dynamic')
     )
 
+
+class UserSubmit(db.Model, ModelMixin):
+    """Таблица заполненных форм сайта."""
+
+    id = db.Column(db.Integer, primary_key=True)
+    fist_name = db.Column(db.String(64), index=True)
+    last_name = db.Column(db.String(64), index=True)
+    email = db.Column(db.String(120), index=True)
+    text = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
 
 class Role(db.Model, FsRoleMixin):
     id = db.Column(db.Integer, primary_key=True)
