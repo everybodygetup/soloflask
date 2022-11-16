6d374e12132f239f9e18e08c01af80b38607c0b2
Artem Belyakov <a9113621595@gmail.com>
2022-11-16 20:32:50 +0300

    Добавил возможность добавления ролей пользователю

--- app/core/views.py
+++ app/core/views.py
@@ -2,7 +2,7 @@ from flask import Blueprint, request, render_template, redirect, flash, g
 from flask_security import current_user, login_required
 from app.extensions import db
 from app.parts.models import SpareParts
-from app.core.models import UserSubmit
+from app.core.models import UserSubmit, user_datastore
 from app.core.forms import KodForm, FeedbackForm
 
 
@@ -12,6 +12,28 @@ core = Blueprint('core', __name__, template_folder='templates')
 @core.before_app_request
 def create_tables():
     db.create_all()
+    user_datastore.find_or_create_role(
+        name="admin",
+        permissions={
+            "admin-read",
+            "admin-write",
+            "user-read",
+            "user-write",
+            "partner-read",
+            "partner-write",
+        },
+    )
+    user_datastore.find_or_create_role(
+        name="monitor", permissions={"admin-read", "user-read"}
+    )
+    user_datastore.find_or_create_role(
+        name="partner", permissions={"partner-read", "partner-write"}
+    )
+    user_datastore.find_or_create_role(
+        name="user", permissions={"user-read", "user-write"}
+    )
+    user_datastore.find_or_create_role(name="reader", permissions={"user-read"})
+    db.session.commit()
     
 
 @core.route('/index', methods=['GET', 'POST'])
